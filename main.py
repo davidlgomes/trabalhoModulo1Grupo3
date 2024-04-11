085672db2ce86763064a4bb29b0b2a78999f3ac0
WillianXG <wilian.mine789@gmail.com>
2024-04-11 17:58:16 -0300

    novas perguntas

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from pesquisaSaude import PesquisaSaude
 
 def main():
     perguntas_saude = [
-        "Você pratica atividade física regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei "
-        "responder:\n>> ",
-        "Você consome pelo menos 5 porções de frutas e vegetais por dia? \n 1 - Sim \n 2 - Não \n 3 - Não sei "
-        "responder:\n>> ",
-        "Você costuma dormir pelo menos 7 horas por noite? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
-        "Você fuma atualmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> "
-    ]
+    "Você pratica atividade física regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você consome pelo menos 5 porções de frutas e vegetais por dia? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você costuma dormir pelo menos 7 horas por noite? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você fuma atualmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você consome bebidas alcoólicas regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você costuma praticar atividades de relaxamento, como meditação ou yoga? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+    "Você costuma realizar check-ups médicos regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> "
+]
 
     pesquisa_saude = PesquisaSaude(perguntas_saude)
 
